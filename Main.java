ac8663ab9e7b96b1a110d8d3daf34b80f1b8f3c9
JhandySelencio <154280174+JhandySelencio@users.noreply.github.com>
2024-11-29 17:16:23 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,26 @@
 public class Main{
-   public static void main(String[] args){
-      Date orderDate = new Date("29", "11", "2024");
-      Customer customer = new Customer();
-      MenuFinalProject finalProject = new MenuFinalProject();
-      
-      String name = finalProject.saveNameText
-      String address =
-      String contactNo = 
-   }
-}
+    public static void main(String[] args){
+       MenuFinalProject fp = new MenuFinalProject();
+       Date date = new Date(29, 11, 2024);
+
+       String name = fp.getSaveNameText();
+       String address = fp.getSaveAddressText();
+       int contactNo = fp.getChangeContactNoText();
+
+       int gallon = fp.getChangeSaveQuantityText();
+       double price = fp.getSavePriceText();
+       int quantity = fp.getChangeSaveQuantityText();
+
+       String type = fp.getGivenText();
+
+       Staff staff = new Staff("Kiervy", "Hernani", "Delivery Man");
+
+       WaterRefillingStation wrs = new WaterRefillingStation(gallon, price, "Water Refilling System: Refill and Go", quantity, date);
+
+       double total = wrs.calculateTotalAmount(price, quantity);
+    
+       Customer customer = new Customer(name, address, contactNo, date);
+
+       wrs.createOrder(name, address, contactNo, date, type, total);
+    }
+ }
