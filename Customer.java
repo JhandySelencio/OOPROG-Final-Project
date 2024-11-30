4c976bff85682154d6a3d5ed29084c4fb93f6bea
JhandySelencio <154280174+JhandySelencio@users.noreply.github.com>
2024-11-30 21:08:15 +0800

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,37 +1,39 @@
 // Made by Raymond Sotelo
-
 public class Customer{
    private String name;
    private String address;
    private int contactNumber;
    private Date date;
    
-   public Customer(String name, String address, int contactNumber, Date date){
-      this.name = name;
-      this.address = address;
-      this.contactNumber = contactNumber;
-      this.date = date;
-   }
-   
    public String getName(){
       return this.name;
+   }
+   
+   public void setName(String name){
+      this.name = name;
    }
    
    public String getAddress(){
       return this.address;
    }
    
+   public void setAddress(String address){
+      this.address = address;
+   }
+   
    public int getContactNumber(){
       return this.contactNumber;
+   }
+   
+   public void setContactNumber(int contactNumber){
+      this.contactNumber = contactNumber;
    }
    
    public Date getDate(){
       return this.date;
    }
    
-   public void displayCustomerInfo(){
-      System.out.println("Name: " + this.name);
-      System.out.println("Address: " + this.address);
-      System.out.println("Contact Number: " + this.contactNumber);     
+   public void setDate(Date date){
+      this.date = date;
    }
 }
